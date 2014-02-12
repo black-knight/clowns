16b9a9cc7143a67e83458d056f359d15a9c6110b
black-knight <dani_ande@yahoo.dk>
2014-02-12 07:41:03 +0100

    Initial Commit

---
+++ Clowns/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Clowns
+//
+
+//  Copyright (c) 2014 Trolls Ahead. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Clowns/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Clowns
+//
+//  Created by Daniel Andersen on 12/02/14.
+//  Copyright (c) 2014 Trolls Ahead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
